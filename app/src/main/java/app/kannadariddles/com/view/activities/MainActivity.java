15238ab1d96f75dc390040d36f399449b8568590
varunam <varunvgnc@gmail.com>
2018-10-23 15:27:33 +0530

    added vertical viewpager skeleton

--- app/src/main/java/app/kannadariddles/com/view/activities/MainActivity.java
+++ app/src/main/java/app/kannadariddles/com/view/activities/MainActivity.java
@@ -1,17 +1,27 @@
 package app.kannadariddles.com.view.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import app.kannadariddles.com.kannadariddles.R;
+import app.kannadariddles.com.view.adapter.ViewPagerAdapter;
+import me.kaelaela.verticalviewpager.transforms.DefaultTransformer;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private ViewPagerAdapter viewpagerAdapter;
+    private ViewPager viewPager;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        viewpagerAdapter = new ViewPagerAdapter(getApplicationContext());
+        viewPager = findViewById(R.id.main_viewpager_id);
+        viewPager.setAdapter(viewpagerAdapter);
+        viewPager.setPageTransformer(false, new DefaultTransformer());
         
     }
 }
---
+++ app/src/main/java/app/kannadariddles/com/view/adapter/ViewPagerAdapter.java
@@ -0,0 +1,47 @@
+package app.kannadariddles.com.view.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.kannadariddles.com.kannadariddles.R;
+
+/**
+ * Created by varun.am on 23/10/18
+ */
+public class ViewPagerAdapter extends PagerAdapter {
+    
+    private Context context;
+    
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+    
+    @Override
+    public int getCount() {
+        return 4;
+    }
+    
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == (ConstraintLayout) o;
+    }
+    
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_riddle, container, false);
+        container.addView(view);
+        
+        return view;
+    }
+    
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ConstraintLayout) object);
+    }
+}
